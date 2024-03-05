0be30dd786b6b9af2cb2e87104a08dd5c2330d4d
Jimi <jimi.denysiak@icloud.com>
2024-03-05 18:38:59 +0100

    INIT

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+print("Welcome to Number Guessing\n Don't Hesitate to follow me on Github : Jimichelle\n")
+#Start With Lower and Upper Ranges
+print("Select your lower then upper range of guessing :")
+lower_number = input("Lower Number : ")
+upper_number = input("Upper Number : ")
+print("Your range of guessing is between " + lower_number + " and " + upper_number)
+random_number = random.randint(int(lower_number),int(upper_number))
+number_guess = int(input("How much guessing you need ? : "))
+print("You choose %d guessing\n" % number_guess)
+count = 0
+print("The Game is starting")
